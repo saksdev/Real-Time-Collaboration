4a3cbfcdddfbc1bc6477b5479caf918d1c9f732b
Saksdev <sakshams346@gmail.com>
2026-07-12 11:11:49 +0530

    fix: add free public TURN servers to resolve mobile-to-laptop NAT traversal connection issues

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -773,7 +773,23 @@ export default function Editor(props: EditorProps) {
           iceServers: [
             { urls: 'stun:stun.l.google.com:19302' },
             { urls: 'stun:stun1.l.google.com:19302' },
-            { urls: 'stun:stun2.l.google.com:19302' }
+            { urls: 'stun:stun2.l.google.com:19302' },
+            { urls: 'stun:stun.nextcloud.com:443' },
+            {
+              urls: 'turn:openrelay.metered.ca:80',
+              username: 'openrelay',
+              credential: 'openrelay'
+            },
+            {
+              urls: 'turn:openrelay.metered.ca:443',
+              username: 'openrelay',
+              credential: 'openrelay'
+            },
+            {
+              urls: 'turns:openrelay.metered.ca:443',
+              username: 'openrelay',
+              credential: 'openrelay'
+            }
           ]
         }
       }
